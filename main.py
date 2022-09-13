64f6dfafa820ae2ae29f2d6a417696c566b75a0f
GiovanniAiello <giovanniaiello1@gmail.com>
2022-09-12 22:04:34 -0500

    merging with new 2010

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ import pandas
 session = requests.Session() #persist parameters and cookiesx
 years1 = range(2006, 2008)
 years2 = range(2008, 2010)
-<<<<<<< HEAD
-years3 = range(2011, 2012)
-=======
-years3 = range(2010, 2022)
->>>>>>> master
+years3 = range(2011, 2022)
 #
 main_url = 'https://www.creditfixings.com{id}'
 urlyears1 = 'https://www.creditfixings.com/CreditEventAuctions/static/credit_event_auction/{year}.shtml'
@@ -33,7 +29,6 @@ for year in years1:
         for table in soup2.find_all('table'):
             print(auction_name)
             print(pandas.read_html(str(table)))
-<<<<<<< HEAD
 
 year = 2008
 html1 = session.get(urlyears1.format(year=year)).text #request the html by year
@@ -88,44 +83,6 @@ for i in range(length):
     for table in soup3.find_all('table'):
         print(auction_name)  #add the name of the company whose CDS are auctioned before each table
         print(pandas.read_html(str(table))) # print all the table
-=======
-
-year=2008
-html1 = session.get(urlyears1.format(year=year)).text #request the html by year
-soup1 = BeautifulSoup(html1, "html.parser")#parse the html
-auctionId = soup1.find_all("span", class_="standalonelink") # import all the anchor that have "holdings.jsp" in their href
-length = len(auctionId)  # how many auction for each year
-for i in range(length):
-    id1 = auctionId[i].find('a')['href'] # now let's extract the link to each auction
-    auction_name = auctionId[i].text
-    html2 = session.get(main_url.format(id=id1)).text # this the auction page and we need to select the link the bring us to the results
-    soup2 = BeautifulSoup(html2, "html.parser")  #parse the html
-    results = soup2.find_all("span", class_="standalonelink")[0]
-    id2 = results.find('a')['href']
-    id2 = id2.replace("dis", "res")
-    html3 = session.get(url2008id.format(id=id2)).text # this the auction page and we need to select the link the bring us to the results
-    soup3 = BeautifulSoup(html3, "html.parser")
-
-    for table in soup3.find_all('table'):
-        print(auction_name)
-        print(pandas.read_html(str(table)))
-
-year=2009
-html1 = session.get(urlyears1.format(year=year)).text #request the html by year
-soup1 = BeautifulSoup(html1, "html.parser")#parse the html
-auctionId = soup1.find_all("span", class_="standalonelink") # import all the anchor that have "holdings.jsp" in their href
-length = len(auctionId)  # how many auction for each year
-for i in range(length):
-    id1 = auctionId[i].find('a')['href'] # now let's extract the link to each auction
-    auction_name = auctionId[i].text
-    id2 = id1.replace("index", "results")
-    html2 = session.get(url2009id.format(id=id2)).text # this the auction page and we need to select the link the bring us to the results
-    soup2 = BeautifulSoup(html2, "html.parser") 
-     #parse the html
-    for table in soup2.find_all('table'):
-        print(auction_name)
-        print(pandas.read_html(str(table)))
->>>>>>> master
 
 
 for year in years3:
@@ -142,10 +99,6 @@ for year in years3:
         id2 = ticker[0].get('href') #it extracts something like "/results.jsp?ticker=ASTL"
         html3 = session.get(urlid.format(id=id2)).text #get to the page with results for each ticker
         soup3 = BeautifulSoup(html3, "html.parser") #parse the page
-<<<<<<< HEAD
-=======
-
->>>>>>> master
         for table in soup3.find_all('table'):
             print(auction_name)  #add the name of the company whose CDS are auctioned before each table
             print(pandas.read_html(str(table))) # print all the table
